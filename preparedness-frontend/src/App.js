f8e765fefb48aebf3b4dbfc7edfee312e05b024e
alopezdelgado <andrealdelgado_99@outlook.com>
2023-10-21 06:55:43 -0700

    added button functionality to click me and reset

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,41 @@
-import React from "react"
+import { useState }from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userName, setUserName] = useState("")
+  
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleNameChange = (event) => {
+    setUserName(event.target.value)
+  }
+
+  const resetNameChange = () => {
+    setUserName("")
+    console.log("Reset button clicked")
+  }
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+          <Input value={userName} onChange={handleNameChange}/>
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <Button onClick={openModal}>Click Me</Button>
+        <Button onClick={resetNameChange}>Reset</Button>
+        {isModalOpen && (
+          <ModalComponent isOpen={isModalOpen} closeModal={closeModal} userName={userName} />
+          )}
       </div>
     </div>
   )
